22840bbbcaa91c556c3beea9482234e265232c68
SameeraMadushan <sameeradelpechithra@gmail.com>
2017-11-23 17:16:42 +0530

    UI update with flow
    Signed-off-by: SameeraMadushan <sameeradelpechithra@gmail.com>

--- src/app/pages/topup/topup.component.ts
+++ src/app/pages/topup/topup.component.ts
@@ -13,17 +13,25 @@ export class TopupComponent implements OnInit {
   ngOnInit() {
   }
   card = '';
-  loggedIn = 'yes';
-  onClick() {
+  loggedIn = true;
+  loggedIn2 = false;
+  password = '';
+  addCredit = false;
+  checkBalance = false;
+  creditByCash = false;
+  creditByCard = false;
+
+  topupLogin() {
     swal.setDefaults({
       input: 'password',
       confirmButtonText: 'Submit',
       showCancelButton: true,
       inputPlaceholder: 'Enter your password'
     })
-    var steps=[
-      'Your user ID: '+this.card+'\nEnter your password'
+    var steps = [
+      'Your user ID: ' + this.card + '\nEnter your password'
     ]
+    let global = this;
     swal.queue(steps).then(function (result) {
       swal.resetDefaults()
       if (result.value == '123') {
@@ -33,11 +41,9 @@ export class TopupComponent implements OnInit {
           showConfirmButton: false,
           timer: 1500
         })
-        console.log('CALLED');
-        this.loggedIn = 'No';
-        console.log(this.loggedIn);
+        global.password = result.value;
       }
-      else if(result.value != '123' && result.value != undefined){
+      else if (result.value != '123' && result.value != undefined) {
         swal({
           title: 'Invalid Login Details! Try again!',
           type: 'error',
@@ -45,6 +51,36 @@ export class TopupComponent implements OnInit {
           timer: 1500
         })
       }
-    })
+    }).then(function () {
+      if (global.password == '123') {
+        global.loggedIn = false;
+        global.loggedIn2 = true;
+      }
+    });
+  }
+
+  addCredits() {
+    this.loggedIn2 = false;
+    this.addCredit = true;
+    this.checkBalance = false;
+
+  }
+
+  chkBalance() {
+    this.loggedIn2 = true;
+    this.checkBalance = true;
+    this.addCredit = false;
+  }
+
+  addCreditsbyCash() {
+    this.loggedIn2 = false;
+    this.creditByCash = true;
+    this.creditByCard = false;
+  }
+
+  addCreditsbyCard() {
+    this.loggedIn2 = false;
+    this.creditByCard = true;
+    this.creditByCash = false;
   }
 }
